cb75fc9aa79ecec56d2fd30c030515206847d857
NicolasDuciaume <47901767+NicolasDuciaume@users.noreply.github.com>
2022-03-08 19:23:50 -0500

    Update ProjectApplication.java

--- src/main/java/SYSC6/Project/ProjectApplication.java
+++ src/main/java/SYSC6/Project/ProjectApplication.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Set;
