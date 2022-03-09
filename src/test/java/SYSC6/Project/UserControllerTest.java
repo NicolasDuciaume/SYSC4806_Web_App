5a4cba100ce5e0a6208cd991d32544245a3fd4ae
nazifatanzim <44646521+nazifatanzim@users.noreply.github.com>
2022-03-08 21:08:39 -0500

    UserControllerTest created but not implemented

---
+++ src/test/java/SYSC6/Project/UserControllerTest.java
@@ -0,0 +1,21 @@
+package SYSC6.Project;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+    //TODO: implement
+    
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void getAllAddressBooks() {
+    }
+
+    @Test
+    void createUser() {
+    }
+}
