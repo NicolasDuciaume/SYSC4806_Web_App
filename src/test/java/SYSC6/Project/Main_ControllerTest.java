3754320a8beb615388b297af6299a103c3e76e01
nazifatanzim <44646521+nazifatanzim@users.noreply.github.com>
2022-03-23 08:29:16 -0400

    Update Main_ControllerTest.java

--- src/test/java/SYSC6/Project/Main_ControllerTest.java
+++ src/test/java/SYSC6/Project/Main_ControllerTest.java
@@ -1,11 +1,49 @@
 package SYSC6.Project;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import org.springframework.ui.Model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Main_ControllerTest {
-    // TODO: Implement tests
+    private MockMvc mockMvc;
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void login_process() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void reg() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void logout() {
+    }
+
+    @Test
+    void greeting() {
+    }
+
     @Test
     void createUser() {
     }
