c3fa8eade377ad66aeefbdcf852e26e684df4eca
Md Aiman Sharif <sharif.aiman@gmail.com>
2022-04-03 15:58:07 -0400

    changes in progres

--- src/main/java/SYSC6/Project/Main_Controller.java
+++ src/main/java/SYSC6/Project/Main_Controller.java
@@ -239,4 +239,38 @@ public class Main_Controller {
         }
         return users;
     }
+
+    public User DelUser(Long id){
+        JSONParser jsonParser = new JSONParser();
+        User user = new User();
+        System.out.println(id);
+        try {
+            URL url = new URL ("https://projectsysc4806.herokuapp.com/rest/api/user/Del/22+id.toString()); " +
+                    "HttpURLConnection con = (HttpURLConnection)url.openConnection()");
+            con.setRequestMethod("DELETE");
+            con.setRequestProperty("Content-Type", "application/json; utf-8");
+            con.setRequestProperty("Accept", "application/json");
+            con.setDoOutput(true);
+
+            try(BufferedReader br = new BufferedReader(
+                    new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8))) {
+                StringBuilder response = new StringBuilder();
+                String responseLine;
+                while ((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+                System.out.println(response);
+                JSONObject temp = (JSONObject) jsonParser.parse(response.toString());
+                //System.out.println(temp.get("username").toString());
+                user = new User(temp.get("username").toString(), temp.get("password").toString());
+
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+        catch (IOException e){
+            System.out.println("Error");
+        }
+        return user;
+    }
 }
